8e579a37806fabe59a78e5c9ba3d283526615201
Manatee <github@osuosl.org>
2020-01-14 00:16:15 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-scponly CHANGELOG
 This file is used to list changes made in each version of the
 osl-scponly cookbook.
 
+1.0.0 (2020-01-14)
+------------------
+- scponly default recipe
+
 0.1.0
 -----
 - Initial release of osl-scponly
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ issues_url       'https://github.com/osuosl-cookbooks/osl-scponly/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-scponly'
 description      'Installs/Configures osl-scponly'
 long_description 'Installs/Configures osl-scponly'
-version          '0.1.0'
+version          '1.0.0'
 
 supports         'centos', '~> 7.0'
 
