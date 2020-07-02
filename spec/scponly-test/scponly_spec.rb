19177b0c685eae658207caf33e8ad2d22065bbcf
Ryan Zimmerman <zimmerry@oregonstate.edu>
2020-07-02 15:52:53 -0700

    Add stub_command

--- spec/scponly-test/scponly_spec.rb
+++ spec/scponly-test/scponly_spec.rb
@@ -15,7 +15,11 @@ describe 'scponly-test::scponly' do
 
       include_examples 'scponly_user', 'scponly_test', '/home/scponly_test'
 
-      it { expect(chef_run).to include_recipe('osl-scponly::default') }
+      it { expect(chef_run).to include_recipe('osl-scponly::default') }\
+
+      before do
+        stub_command('grep scponly /etc/passwd')
+      end
 
       it do
         expect(chef_run).to create_scponly_user('scponly_test').with(
