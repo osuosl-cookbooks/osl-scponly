c88eb62029cd24eea7c9332862be1b6b9f7a021f
Ryan Zimmerman <zimmerry@oregonstate.edu>
2020-07-02 14:46:34 -0700

    Cookstyle fixes

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ issues_url       'https://github.com/osuosl-cookbooks/osl-scponly/issues'
 license          'Apache-2.0'
 chef_version     '>= 14.0'
 description      'Installs/Configures osl-scponly'
-long_description 'Installs/Configures osl-scponly'
 version          '1.0.2'
 
 supports         'centos', '~> 7.0'
--- test/cookbooks/scponly-test/metadata.rb
+++ test/cookbooks/scponly-test/metadata.rb
@@ -6,7 +6,6 @@ chef_version     '>= 13'
 issues_url       'https://github.com/osuosl-cookbooks/osl-scponly/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-scponly'
 description      'Tests osl-scponly'
-long_description 'Tests osl-scponly'
 version          '0.1.0'
 
 supports         'centos', '~> 7.0'
