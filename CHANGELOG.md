9ab1405780212d4033068dd958aa68e0a81bf090
Manatee <github@osuosl.org>
2020-09-03 22:25:20 +0000

    Automatic minor-level version bump to v1.2.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-scponly CHANGELOG
 This file is used to list changes made in each version of the
 osl-scponly cookbook.
 
+1.2.0 (2020-09-03)
+------------------
+- Chef 16 Fixes
+
 1.1.0 (2020-07-13)
 ------------------
 - Chef 15 Fixes
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/osl-scponly/issues'
 license          'Apache-2.0'
 chef_version     '>= 14.0'
 description      'Installs/Configures osl-scponly'
-version          '1.1.0'
+version          '1.2.0'
 
 supports         'centos', '~> 7.0'
 
