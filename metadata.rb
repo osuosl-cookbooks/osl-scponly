d191b2ae52609275880a1c34a9d99b0c2ee3c02c
Frank Nguyen <frankreed@osuosl.edu>
2022-03-03 18:27:12 -0800

    replaced centos 8 with centos stream

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ description      'Installs/Configures osl-scponly'
 version          '1.5.0'
 
 supports         'centos', '~> 7.0'
-supports         'centos', '~> 8.0'
+supports         'centos_stream', '~> 8.0'
 
 depends          'line'
 depends          'osl-repos'
