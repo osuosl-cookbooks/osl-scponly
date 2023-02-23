d462c9c7bd7894a077a4b625aaf7f9f571fe889c
Justin Fernbaugh <fernbaughj@gmail.com>
2023-02-23 12:42:56 -0800

    Added support for Alma Linux 8

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,7 @@ chef_version     '>= 16.0'
 description      'Installs/Configures osl-scponly'
 version          '1.5.0'
 
+supports         'almalinux', '~> 8.0'
 supports         'centos', '~> 7.0'
 supports         'centos', '~> 8.0'
 
