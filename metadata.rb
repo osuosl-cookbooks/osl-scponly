6e4724efcb2f5102b59a8cf0cb64685de9b0ab56
Justin Fernbaugh <fernbaughj@gmail.com>
2023-04-13 12:34:58 -0700

    Added support for CentOS Stream 8

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ version          '1.5.0'
 
 supports         'almalinux', '~> 8.0'
 supports         'centos', '~> 7.0'
-supports         'centos', '~> 8.0'
+supports         'centos_stream', '~> 8.0'
 
 depends          'line'
 depends          'osl-repos'
