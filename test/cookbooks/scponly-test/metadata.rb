5edbbad2a9efd1193e765bb093e6f5feba4bca72
Lance Albertson <lance@osuosl.org>
2024-07-17 16:02:28 -0700

    Remove support for CentOS 7
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/cookbooks/scponly-test/metadata.rb
+++ test/cookbooks/scponly-test/metadata.rb
@@ -8,6 +8,6 @@ source_url       'https://github.com/osuosl-cookbooks/osl-scponly'
 description      'Tests osl-scponly'
 version          '0.1.0'
 
-supports         'centos', '~> 7.0'
+supports         'almalinux', '~> 8.0'
 
 depends          'osl-scponly'
